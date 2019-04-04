755682adc41e1b02c1cffa3866e3891422c05621
stred13 <34312900+stred13@users.noreply.github.com>
2019-04-04 13:53:52 +0700

    cm1
    tạo màn hình chính
    màn hình tạo ví tiền

---
+++ app/src/main/java/com/example/moneymanagement_android/budget_creating.java
@@ -0,0 +1,13 @@
+package com.example.moneymanagement_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class budget_creating extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_creating);
+    }
+}

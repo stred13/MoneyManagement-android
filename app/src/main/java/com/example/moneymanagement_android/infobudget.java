460b5aced0ee73596b35f33a747a01ebd2cd6dc4
dangvantien <dangvantien1808@gmail.com>
2019-06-05 22:25:15 +0700

    nested recyclerview

--- app/src/main/java/com/example/moneymanagement_android/infobudget.java
+++ app/src/main/java/com/example/moneymanagement_android/infobudget.java
@@ -1,40 +1,32 @@
 package com.example.moneymanagement_android;
 
-import android.app.Dialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.example.moneymanagement_android.fragments.BudgetFragment;
+import com.example.moneymanagement_android.fragments.ExpenseBudgetFragment;
 import com.example.moneymanagement_android.fragments.ExpenseFragment;
 import com.example.moneymanagement_android.fragments.IncomeFragment;
-import com.example.moneymanagement_android.fragments.StatisticFragment;
 import com.example.moneymanagement_android.models.budget;
 import com.example.moneymanagement_android.viewmodels.IncomeViewModel;
 import com.example.moneymanagement_android.viewmodels.budgetViewModel;
 import com.example.moneymanagement_android.viewmodels.expenseViewModel;
 
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class infobudget extends AppCompatActivity {
 
     private ExpenseFragment expenseFragment;
     private IncomeFragment incomeFragment;
+    private ExpenseBudgetFragment expenseBudgetFragment;
     private ViewPager mainViewInfo;
     private TabLayout tabLayoutInfo;
 
@@ -87,10 +79,12 @@ public class infobudget extends AppCompatActivity {
 
         expenseFragment = new ExpenseFragment();
         incomeFragment = new IncomeFragment();
+        expenseBudgetFragment = new ExpenseBudgetFragment();
 
         ViewPaperAdapter viewPaperAdapter = new ViewPaperAdapter(getSupportFragmentManager());
-        viewPaperAdapter.addFragment(expenseFragment, "Chi Tiêu");
-        viewPaperAdapter.addFragment(incomeFragment, "Thu Nhập");
+        //viewPaperAdapter.addFragment(expenseFragment, "Chi Tiêu 333");
+        //viewPaperAdapter.addFragment(incomeFragment, "Thu Nhập");
+        viewPaperAdapter.addFragment(expenseBudgetFragment, "Chi tiêu");
 
         mainViewInfo.setAdapter(viewPaperAdapter);
         tabLayoutInfo.setupWithViewPager(mainViewInfo);
@@ -100,8 +94,6 @@ public class infobudget extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
                 Intent i = new Intent(getApplication(), expense_creating.class);
                 startActivity(i);
             }
